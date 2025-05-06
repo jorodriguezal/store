1c308f22a7a8335edd2594125e1cdcbe89a0dec0
Johan Rodriguez <jorodriguezal@unal.edu.co>
2025-05-06 14:23:07 +0200

    feat: :sparkles: add first CRUD model

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,18 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root "products#index"
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :products # Creates the whole RESTful routes for the products resource. It does the same as the commented lines below.
+  # get "/products", to: "products#index"
+  # get "/products/new", to: "products#new"
+  # post "/products", to: "products#create"
+  # get "/products/:id", to: "products#show"
+  # get "/products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
+
+  # delete "/products/:id", to: "products#destroy"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
