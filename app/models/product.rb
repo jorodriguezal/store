ea1e921a794ca111f14c1a9d0debfe9fc9fe4a6e
Johan Rodriguez <jorodriguezal@unal.edu.co>
2025-05-06 15:18:55 +0200

    feat: :sparkles: add possibility to attach images to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+    has_one_attached :featured_image # This line allows the Product model to have one attached image, which is typically used for a featured image.
     has_rich_text :description # This line adds rich text functionality to the description attribute of the Product model.
     validates :name, presence: true
 end
