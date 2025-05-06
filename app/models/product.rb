a36894ed3fc3570ddc2139152a60f427b1011165
Johan Rodriguez <jorodriguezal@unal.edu.co>
2025-05-06 15:52:05 +0200

    feat: :sparkles: add inventory counter

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
     has_one_attached :featured_image # This line allows the Product model to have one attached image, which is typically used for a featured image.
     has_rich_text :description # This line adds rich text functionality to the description attribute of the Product model.
     validates :name, presence: true
+    validates :inventory_count, numericality: { greater_than_or_equal_to: 0 } # This line validates that the inventory_count attribute is a number and is greater than or equal to 0.
+
 end
