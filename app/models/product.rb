6c50b92ed421aeabeb5d25523154682612c02704
Johan Rodriguez <jorodriguezal@unal.edu.co>
2025-05-06 15:12:34 +0200

    feat: :sparkles: add rich text functionalities

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
+    has_rich_text :description # This line adds rich text functionality to the description attribute of the Product model.
     validates :name, presence: true
 end
